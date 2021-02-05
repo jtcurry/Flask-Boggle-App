e66a9d7014638d8116c2062437fdcf7d72b32f50
John Curry <jtc3852@gmail.com>
2021-02-05 09:01:25 -0500

    Add score keeping on front end

--- static/boggle.js
+++ static/boggle.js
@@ -1,11 +1,22 @@
 
 const wordSubmit = document.getElementById("wordsubmit");
 const displayMessage = document.getElementById("displaymessage");
+const displayScore = document.getElementById("displayscore");
 
+let totalScore = 0;
+
+// CALCULATE SCORE FROM A VALID WORD
+const calculateWordScore = (currentWord) => {
+  wordScore = currentWord.length;
+  return wordScore;
+}
+
+
+
+
+
+// HANDLE SUBMISSION OF A WORD
 wordSubmit.addEventListener("submit", handleSubmit);
-
-
-
 
 async function handleSubmit(event) {
   event.preventDefault();
@@ -13,15 +24,14 @@ async function handleSubmit(event) {
   let response = await axios.get("/submitted_word", {params: {word:submission}});
   let responseMessage = response.data.result
 
-  //TODO ERASE BELOW TESTING
-  console.log(response);
-  
   if (responseMessage == "not-word") {
     displayMessage.textContent = `${submission} is not a word, try again!`
   } else if (responseMessage == "not-on-board") {
     displayMessage.textContent = `${submission} is not on the board, try again!`
   } else if (responseMessage == "ok") {
-    displayMessage.textContent = "THIS IS A WORD!!!!!!! **erase"
+    totalScore += calculateWordScore(submission);
+    displayMessage.textContent = "THIS IS A WORD, DO SOMETHING!!!!!!! **erase**";
+    displayScore.textContent = totalScore;
   }
 }
 
